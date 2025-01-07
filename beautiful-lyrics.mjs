d7c377ccc19e71f96b8c8db1ee36cfa4a469d9ee
Ахтем Махтиев <makhtiev_ar@sima-land.ru>
2025-01-07 22:59:48 +0500

    Initial commit

---
+++ beautiful-lyrics.mjs
@@ -0,0 +1,25 @@
+const version = "4.2.0";
+const repository = "https://cdn.jsdelivr.net/gh/iHelops/beautiful-lyrics-bundle@main";
+
+// Wait for Spicetify/Snackbar to load
+await new Promise((resolve) => {
+  const interval = setInterval(() => {
+    if (Spicetify !== undefined && Spicetify.Snackbar !== undefined) {
+      clearInterval(interval);
+      resolve();
+    }
+  }, 10);
+});
+
+const load = () => {
+  // Load CSS
+  const css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.href = `${repository}/releases/${version}/bundle.css`;
+  document.body.appendChild(css);
+
+  // Load JS
+  import(`${repository}/releases/${version}/bundle.mjs`);
+};
+
+load();
